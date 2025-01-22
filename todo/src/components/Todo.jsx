1b4226f0b306fe9474831a0f389b63c23ce00ff4
Winoliya Ponsingh <win.sin.rt23@dypatil.edu>
2025-01-22 12:18:45 +0530

    Made UI of Input Box

--- todo/src/components/Todo.jsx
+++ todo/src/components/Todo.jsx
@@ -12,6 +12,14 @@ const Todo = () => {
 <h1 className='text-2xl font-semibold '>To-Do List</h1>
 </div>
 
+{/*--input box-- */}
+
+<div className=' flex items-center bg-gray-200 my-8 rounded-full'>
+
+  <input className='border-0 placeholder:text-slate-600 bg-transparent h-14 pl-6 pr-2 flex-1' type="text" placeholder='Add your Task'/>
+  <button className='border-0 rounded-full bg-orange-800 h-14 text-white w-32 text-lg font-medium cursor-pointer'>ADD +</button>
+</div>
+
 
     </div>
   )
